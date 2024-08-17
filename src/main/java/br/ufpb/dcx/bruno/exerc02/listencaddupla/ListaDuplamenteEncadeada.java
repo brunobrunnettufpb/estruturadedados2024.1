e7916a5dbcc17f0ff9640733e766f297cd822eff
EvilLink <evillink@live.com>
2024-08-17 18:36:59 -0300

    exerc revisão

--- src/main/java/br/ufpb/dcx/bruno/exerc02/listencaddupla/ListaDuplamenteEncadeada.java
+++ src/main/java/br/ufpb/dcx/bruno/exerc02/listencaddupla/ListaDuplamenteEncadeada.java
@@ -4,12 +4,28 @@ public class ListaDuplamenteEncadeada {
     public Node head;
 
     public void add(Node node) {
+        if (head == null) head = node;
+        else {
+            Node atual = head;
+            while (atual.proximo != null) {
+                atual = atual.proximo;
+            }
 
+            atual.proximo = node;
+            atual.proximo.anterior = atual;
+        }
     }
 
     //QUESTÃO 16
     public void addInicio(Node node) {
+        if (head == null) head = node;
+        else {
+            Node atual = head;
+            atual.anterior = node;
+            node.proximo = atual;
 
+            head = atual;
+        }
     }
 
     //QUESTÃO 17
