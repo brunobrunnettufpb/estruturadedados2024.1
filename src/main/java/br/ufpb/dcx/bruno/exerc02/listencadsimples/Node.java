456828622432951b3be771ce363db324d955572d
EvilLink <evillink@live.com>
2024-08-17 14:27:19 -0300

    SimpleLinkedList

---
+++ src/main/java/br/ufpb/dcx/bruno/exerc02/listencadsimples/Node.java
@@ -0,0 +1,11 @@
+package br.ufpb.dcx.bruno.exerc02.listencadsimples;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int dados) {
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/bruno/exerc02/listencadsimples/SimpleLinkedList.java
@@ -0,0 +1,18 @@
+package br.ufpb.dcx.bruno.exerc02.listencadsimples;
+
+public class SimpleLinkedList {
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) head = newNode;
+        else {
+            Node current = head;
+            while (current != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+}
